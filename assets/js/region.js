acb7447c57238210be761acf699dd892505ab814
ernst01 <thiebaud.ernstberger@gmail.com>
2016-07-05 10:31:44 -0700

    Fix - Customer account address

--- assets/js/region.js
+++ assets/js/region.js
@@ -164,3 +164,47 @@ $('.form-billing-region, .form-horizonal').cascadingDropdown({
     }
     ]
 });
+
+// Customer address
+
+$('.form-region-customer, .form-horizonal').cascadingDropdown({
+    selectBoxes: [
+    {
+        selector: 'select[id$=country]',
+        source: function(request, response) {
+            $.getJSON(acendaBaseUrl + '/api/region', request, function(data) {
+                var country = $('[name$=\\[country_select\\]]').val();
+                if (country == undefined || country == '') {
+                    country = 'US';
+                }
+                response($.map(data.result, function(item, index) {
+                    return {
+                        label: item.label,
+                        value: item.value,
+                        selected: item.value.indexOf(country) != -1
+                    };
+                }));
+            });
+        }
+    },
+    {
+        selector: 'select[id$=state]',
+        requires: ['select[id$=country]'],
+        source: function(request, response) {
+            $.getJSON(acendaBaseUrl + '/api/region/states/'+$('select[id$=country]').val(), request, function(data) {
+                var state = $('[name$=\\[state_select\\]]').val();
+                if (state == undefined || state == '') {
+                    state = 'CA';
+                }
+                response($.map(data.result, function(item, index) {
+                    return {
+                        label: item.label,
+                        value: item.value,
+                        selected: item.value.indexOf(state) != -1
+                    };
+                }));
+            });
+        },
+    }
+    ]
+});
