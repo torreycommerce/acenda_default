89ab9d547974cca841bea0c66db8b4a8f5a58380
Jack Anderson <automatic.anderson@gmail.com>
2017-05-16 16:51:47 -0700

    fleshed out some of the forms and some of the data models

---
+++ assets/js/checkoutbb/models/cart.js
@@ -0,0 +1,10 @@
+var checkout = checkout || {};
+
+(function () {
+	'use strict';
+	checkout.Cart = Backbone.Model.extend({
+
+
+
+	});
+})();
---
+++ assets/js/checkoutbb/models/order.js
@@ -0,0 +1,11 @@
+var checkout = checkout || {};
+
+(function () {
+	'use strict';
+	checkout.Order = Backbone.Model.extend({
+
+
+
+
+	});
+})();
