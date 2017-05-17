ad8c78fdf4331627ad2b25fbda6211705bd54fbd
Jack Anderson <automatic.anderson@gmail.com>
2017-05-17 11:09:53 -0700

    filled out the rest of the forms.. moved sticky summary to backbone view

--- assets/js/checkoutbb/checkoutbb.js
+++ assets/js/checkoutbb/checkoutbb.js
@@ -1,11 +1,6 @@
 var checkout = checkout || {};
-var ENTER_KEY = 13;
-var ESC_KEY = 27;
 
 $(function () {
 	'use strict';
 	new checkout.CheckoutView();
 });
-
-
-$('#summary-panel').stick_in_parent();
--- assets/js/checkoutbb/views/checkout-view.js
+++ assets/js/checkoutbb/views/checkout-view.js
@@ -10,6 +10,15 @@ var checkout = checkout || {};
 		},
 		initialize: function () {
 		    console.log('initializing checkout');
+	    
+		    var fixSummaryHeight = function() {
+		    	$('#checkout-right').height($('#checkout-left').height());
+				console.log('updating height');
+                $(document.body).trigger("sticky_kit:recalc");				
+				setTimeout(fixSummaryHeight,1000);		    	
+		    }
+		    fixSummaryHeight();
+			$('#summary-panel').stick_in_parent({recalc_every: 1});			    
 		},
 		render: function () {
 		    console.log('rendering checkout');		
