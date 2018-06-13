687131ac1578ff792cb65ba765b2b998af06d936
Lee A <leea@torreycommerce.com>
2018-06-13 20:26:48 +0700

    re-work parsley / form validation, forms, lonely-forms, acct pages

--- assets/js/site-parsley.js
+++ assets/js/site-parsley.js
@@ -1,41 +1,29 @@
 var limit_feed = ["text", "tel", "email", "password", "url", "datetime", "time", "number"];
 
-	$('form').each(function() {
-		$(this).parsley({
-			successClass: 'has-success',
-			errorClass: 'has-error',
-			errors: {
-				classHandler: function(el) {
-					return el.parent();
-				},
-				errorsWrapper: '',
-				errorElem: ''
+$('form').each(function() {
+	$(this).parsley({
+		successClass: 'is-valid',
+		errorClass: 'is-invalid',
+		errors: {
+			classHandler: function(el) {
+				return el;
 			},
-			listeners: {
-				onFieldError: function ( elem, constraints, ParsleyField ) {
-					if (!elem.parents('.form-group').hasClass('has-feedback'))
-						elem.parents('.form-group').addClass('has-feedback');
+			errorsWrapper: '',
+			errorElem: ''
+		},
+		listeners: {
+			onFieldError: function ( elem, constraints, ParsleyField ) {
+				if (!elem.parents('form').hasClass('was-validated'))
+					elem.parents('form').addClass('was-validated');
 
-					if ($.inArray(elem[0].type, limit_feed) >= 0){
-					  if (elem.parent().find(".fa"))
-						  elem.parent().find(".fa").remove();
-					  elem.after('<span class="fa fa-exclamation-triangle form-control-feedback"></span>');
-					}
-
-					elem.attr("data-original-title", elem[0].validationMessage);
-					elem.tooltip("show");
-				},
-				onFieldSuccess: function ( elem, constraints, ParsleyField ) {					
-					if (!elem.parents('.form-group').hasClass('has-feedback'))
-						elem.parents('.form-group').addClass('has-feedback');
-					if ($.inArray(elem[0].type, limit_feed) >= 0){
-					  if (elem.parent().find(".fa"))
-						  elem.parent().find(".fa").remove();
-					  elem.after('<span class="fa fa-ok form-control-feedback"></span>');
-					}
-
-					elem.tooltip("dispose");
-				}
+				elem.attr("data-original-title", elem[0].validationMessage);
+				elem.tooltip("show");
+			},
+			onFieldSuccess: function ( elem, constraints, ParsleyField ) {
+				if (!elem.parents('form').hasClass('was-validated'))
+					elem.parents('form').addClass('was-validated');
+				elem.tooltip("dispose");
 			}
-		});
+		}
 	});
+});
