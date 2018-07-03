71706c0261a0ec630e5a6c85dbe3820323b40f90
Eyal Stern <eyals@acenda.com>
2018-07-03 10:17:32 -0700

    Changes to customer error messages in email sub.

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,3 +1,4 @@
+
 function validateEmailAddress(email) {
 	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return re.test(email);
@@ -10,7 +11,10 @@ $('#SignupButton').click(function() {
 	var email = $('#SignupInput').val();
 	if (!email || !validateEmailAddress(email)) {
 		$('#SignupInput').addClass('has_error');
-		$('html').append('<div class="flash-note affix alert alert-danger"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>Please enter a valid email address.</div>');
+		$('.newsletter-response').load(acendaBaseUrl+'/account/alerts #sub_fail1', function() {
+			console.log('got fail1');
+		});
+		//$('html').append('<div class="flash-note affix alert alert-danger"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>Please enter a valid email address.</div>');
 		return false;
 	}
 	$(this).prop('disabled', true).addClass('wait');
@@ -19,21 +23,36 @@ $('#SignupButton').click(function() {
 		//verify:true
 	}).done(function(response) {
 		$('#SignupButton').prop('disabled', false).removeClass('wait');
-		$('html').append('<div class="flash-note affix alert alert-success"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>Thank you for signing up.</div>');
+		$('.newsletter-response').load(acendaBaseUrl+'/account/alerts #sub_success', function() {
+			console.log('got success');
+		});
+		//$('html').append('<div class="flash-note affix alert alert-success"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>Thank you for signing up.</div>');
 		console.log(response);
 	}).fail(function(response) {
 		var error = 'undefined error';
 		$('#SignupButton').prop('disabled', false).removeClass('wait');
+		
 		console.log(response.responseJSON.error.email[0]);
+		var errortype = response.responseJSON.error.errortype;
+		
 		if (typeof response.responseJSON.error.email[0] != 'undefined') {
 			error = response.responseJSON.error.email[0];
-			var errortype = response.responseJSON.error.errortype;
-			if (typeof  errortype != 'undefined' &&  errortype === 'briteverify' ) {
-				error = typeof email_verify_message !== 'undefined' ? email_verify_message : error;
-			}
 		}
+		
 		$('#SignupInput').addClass('has_error');
 		
-		$('html').append('<div class="flash-note affix alert alert-danger"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'+ error +'</div>');
+		if (typeof errortype === 'string' ) {
+			$('.newsletter-response').load(acendaBaseUrl+'/account/alerts #sub_fail_' + errortype, function() {
+				if ($('.newsletter-response').is(':empty')) {
+					$('html').append('<div class="flash-note affix alert alert-danger"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'+ error +'</div>');
+				}
+			});
+		}
+		else
+		{
+			$('html').append('<div class="flash-note affix alert alert-danger"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'+ error +'</div>');
+		}
 	});
 });
+
+
