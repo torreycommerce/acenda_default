854c63b2ce9650efe44eb4767130d5a36cd7e699
aninit <leea@torreycommerce.com>
2021-01-26 09:09:32 +0700

    Repair has_stock logic for disabled add to cart

--- assets/js/product.js
+++ assets/js/product.js
@@ -311,7 +311,7 @@ function VariantsManager (product, img, isCollection) {
 				$(quantityInput).val(1);
 			}
 			//
-			if(filteredVariants[0].has_stock == '0' || this.getNumber(filteredVariants[0].price) <= 0) {
+			if(filteredVariants[0].has_stock != true || this.getNumber(filteredVariants[0].price) <= 0) {
 				this.disabled = true;
 				this.disableAddToCart(true);
 
