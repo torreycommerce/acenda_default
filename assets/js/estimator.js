95e1bf39bf048ee4f41eb3a3df2f92eb4ff5849d
Ahmet TOK <ahmet@zuzu.dev>
2021-04-09 20:09:06 +0300

    shipping reates by weight

--- assets/js/estimator.js
+++ assets/js/estimator.js
@@ -4,43 +4,40 @@ function getTaxEstimates() {
     var shipping_method = $('[name="shipping_method"]:checked').val();
     var shipping_country = 'US';
     var shipping_state = $('[name="cart[state]"]').val();
-
-    if(!zip_code) {  
-          $('#summary-shipping,#summary-before-tax,#summary-tax').hide();
+    if (!zip_code) {
+        $('#summary-shipping,#summary-before-tax,#summary-tax').hide();
         //  return;
     } else {
-           $('#summary-shipping').show();       
+        $('#summary-shipping').show();
     }
-    if(!zip_code) {
+    if (!zip_code) {
         $('#summary-before-tax,#summary-tax').hide();
     } else {
-
         $('#summary-before-tax,#summary-tax').show();
     }
-    $.post(acendaBaseUrl + '/api/cart',{
-        'shipping_method':shipping_method,
-        'shipping_country':shipping_country,
-        'shipping_state':shipping_state,
-        'shipping_zip':zip_code
+    $.post(acendaBaseUrl + '/api/cart', {
+        'shipping_method': shipping_method,
+        'shipping_country': shipping_country,
+        'shipping_state': shipping_state,
+        'shipping_zip': zip_code
     }, 'json')
-    .done(function() {
-        if(typeof updateCartTotals !== 'undefined') {
-            updateCartTotals($(''),0);
-        }
-       getDeliveryEstimates(shipping_methods);        
-    });
-    
+        .done(function () {
+            if (typeof updateCartTotals !== 'undefined') {
+                updateCartTotals($(''), 0);
+            }
+            getDeliveryEstimates(shipping_methods);
+        });
     return false;
 }
 function getDeliveryEstimates(shipping_methods) {
-    if(!$('[name="cart[shipping_zip]"]').val()) return;
-    shipping_methods.forEach(function(method) {
+    if (!$('[name="cart[shipping_zip]"]').val()) return;
+    shipping_methods.forEach(function (method) {
         console.log('getting estimate for ' + method.id);
-        $('tr[data-method="' +  method.id + '"] .spinner').show();   
-        $.get(acendaBaseUrl + '/api/shippingtools/deliveryestimates/?carrier='+method.carrier_name).done(function(data) {
+        $('tr[data-method="' + method.id + '"] .spinner').show();
+        $.get(acendaBaseUrl + '/api/shippingtools/deliveryestimates/?carrier=' + method.carrier_name).done(function (data) {
             var estimates = data.result;
-            $.each(estimates,function(ek,estimate){
-                if(typeof estimate.estimate == 'undefined') return;
+            $.each(estimates, function (ek, estimate) {
+                if (typeof estimate.estimate == 'undefined') return;
                 var estimate_string = moment(estimate.estimate).calendar(null, {
                     sameDay: 'By [Today]',
                     nextDay: 'By [Tomorrow], MM/DD',
@@ -50,48 +47,63 @@ function getDeliveryEstimates(shipping_methods) {
                     sameElse: 'By MM/DD/YYYY'
                 });
             });
-        }).always(function(){
-            $('tr[data-method="' +  method.id + '"] .spinner').hide();
+        }).always(function () {
+            $('tr[data-method="' + method.id + '"] .spinner').hide();
         })
     });
 }
 function refreshShippingMethods() {
-    if(typeof cartData === 'undefined' || cartData == null) {
-        setTimeout(refreshShippingMethods,200);
+    if (typeof cartData === 'undefined' || cartData == null) {
+        setTimeout(refreshShippingMethods, 200);
         return;
     }
-    var shipping_method_tpl = _.template($('#shipping-methods-template').html()); 
-    $.get(acendaBaseUrl + '/api/shippingmethod/byregion?country=US', function(data) {
-        var defer_methods = [];
-        shipping_methods = data.result;   
-        shipping_methods.forEach(function(method,k) {
-            defer_methods[k]=$.post(acendaBaseUrl + '/api/shippingmethod/' + method.id +  '/rate',{total: cartData.subtotal,quantity: cartData.item_count}, function(response) {
-                shipping_methods[k].price = response.result.rate;
-            })
+    var shipping_method_tpl = _.template($('#shipping-methods-template').html());
+    var defer_methods = [];
+    if (shippingByWeight) {
+        shipping_methods = cartData.shipping_methods;
+        shipping_methods.forEach(function (method, k) {
+            shipping_methods[k].bottom_days_range = method.bottom_days_range.toString();
+            shipping_methods[k].carrier_method = "Carrier Method";
+            shipping_methods[k].carrier_name = "Carrier Method";
+            shipping_methods[k].delivery_estimates = "manual";
+            shipping_methods[k].price = method.shipping_rate;
+            shipping_methods[k].status = 'active';
+            shipping_methods[k].top_days_range = method.top_days_range.toString();
+            shipping_methods[k].rule_by = 'bottom_price';
+            defer_methods.push(k);
+            defer_methods[k].rate = method.shipping_rate;
+            defer_methods[k].date_range = [method.shipping_estimate_start, method.shipping_estimate_end];
         });
-
-        $.when.apply($,defer_methods).then(function() {
-            $('#shipping-method-selection').html(shipping_method_tpl({methods: shipping_methods, current_method: cartData.shipping_method}));
-            getDeliveryEstimates(shipping_methods);               
-            $('[name="shipping_method"]').change(function() {
-                var shipping_method = $('[name="shipping_method"]:checked').val();
-                $.post(acendaBaseUrl + '/api/cart',{
-                    'shipping_method':shipping_method
-                }, 'json')
-                .done(function() {
-                    if(typeof updateCartTotals !== 'undefined') {
-                        updateCartTotals($(''),0);                        
-                    }    
+    } else {
+        $.get(acendaBaseUrl + '/api/shippingmethod/byregion?country=US', function (data) {
+            shipping_methods = data.result;
+            shipping_methods.forEach(function (method, k) {
+                defer_methods[k] = $.post(acendaBaseUrl + '/api/shippingmethod/' + method.id + '/rate', { total: cartData.subtotal, quantity: cartData.item_count }, function (response) {
+                    shipping_methods[k].price = response.result.rate;
+                })
+            });
+        });
+    }
+    $.when.apply($, defer_methods).then(function () {
+        $('#shipping-method-selection').html(shipping_method_tpl({ methods: shipping_methods, current_method: cartData.shipping_method }));
+        getDeliveryEstimates(shipping_methods);
+        $('[name="shipping_method"]').change(function () {
+            var shipping_method = $('[name="shipping_method"]:checked').val();
+            $.post(acendaBaseUrl + '/api/cart', {
+                'shipping_method': shipping_method
+            }, 'json')
+                .done(function () {
+                    if (typeof updateCartTotals !== 'undefined') {
+                        updateCartTotals($(''), 0);
+                    }
                 });
-    
-            });            
         });
     });
 }
-$(document).ready(function() {
+$(document).ready(function () {
     refreshShippingMethods();
 });
-$('#cart_Estimate').click(function(e) {
-   e.preventDefault();
-   getTaxEstimates();   
+$('#cart_Estimate').click(function (e) {
+    e.preventDefault();
+    getTaxEstimates();
 });
--- assets/js/shop-qty.js
+++ assets/js/shop-qty.js
@@ -2,112 +2,121 @@
 var cartData = null;
 function updateCartTotals(qtyField, cartItemId) {
 	$.getJSON(acendaBaseUrl + '/api/sessioncart')
-	.always(function() {
-	})
-	.done(function(data) {	
-		var dataQty = 0.0;		
-		if(typeof data.result.items[cartItemId] !== 'undefined') {
-			dataQty = data.result.items[cartItemId].quantity;	
-		}
+		.always(function () {
+		})
+		.done(function (data) {
+			var dataQty = 0.0;
+			if (typeof data.result.items[cartItemId] !== 'undefined') {
+				dataQty = data.result.items[cartItemId].quantity;
+			}
 
-		var v2Data = qtyField.parents('.item');
-		var priceElement = v2Data.find('.cart-indiv .price .val').html();
+			var v2Data = qtyField.parents('.item');
+			var priceElement = v2Data.find('.cart-indiv .price .val').html();
 
-		cartData = data.result;
+			cartData = data.result;
 
-		amount = parseFloat(priceElement * dataQty).toFixed(2);
-		var savings = parseFloat( ( v2Data.find('.cart-indiv .price-compare .val').html() - priceElement ) * dataQty).toFixed(2);
-		//
-		if ($(v2Data).attr('data-sid')) {
-			var dLID = $(v2Data).attr('data-sid');
-		} else {
-			var dLID = $(v2Data).attr('data-id');
-		}
-		var oldQty = v2Data.find('.cart-total .price .val').text() / priceElement;
-		var changeQty = parseInt(dataQty - oldQty);
-		var absChangeQty = Math.abs(changeQty);
-		//
-		//
-		if (typeof dataLayer !== "undefined") {
-			if (dataLayer !== null) {
-				if (oldQty < dataQty) {
-					//console.log('dataLayer add');
-					dataLayer.push({
-						'event': 'addToCart',
-						'ecommerce': {
-							'currencyCode': 'USD',
-							'add': {                                // 'add' actionFieldObject measures.
-								'products': [{                        //  adding a product to a shopping cart.
-									'name': $(v2Data).find('h3').text(),
-									'id': dLID,
-									'price': priceElement,
-									'brand': $(v2Data).find('.brand').text(),
-									//'category': 'Apparel',
-									//'variant': $(v2Data).attr('data-vid'),
-									'quantity': absChangeQty
-								}]
+			amount = parseFloat(priceElement * dataQty).toFixed(2);
+			var savings = parseFloat((v2Data.find('.cart-indiv .price-compare .val').html() - priceElement) * dataQty).toFixed(2);
+			//
+			if ($(v2Data).attr('data-sid')) {
+				var dLID = $(v2Data).attr('data-sid');
+			} else {
+				var dLID = $(v2Data).attr('data-id');
+			}
+			var oldQty = v2Data.find('.cart-total .price .val').text() / priceElement;
+			var changeQty = parseInt(dataQty - oldQty);
+			var absChangeQty = Math.abs(changeQty);
+			//
+			//
+			if (typeof dataLayer !== "undefined") {
+				if (dataLayer !== null) {
+					if (oldQty < dataQty) {
+						//console.log('dataLayer add');
+						dataLayer.push({
+							'event': 'addToCart',
+							'ecommerce': {
+								'currencyCode': 'USD',
+								'add': {                                // 'add' actionFieldObject measures.
+									'products': [{                        //  adding a product to a shopping cart.
+										'name': $(v2Data).find('h3').text(),
+										'id': dLID,
+										'price': priceElement,
+										'brand': $(v2Data).find('.brand').text(),
+										//'category': 'Apparel',
+										//'variant': $(v2Data).attr('data-vid'),
+										'quantity': absChangeQty
+									}]
+								}
 							}
-						}
-					});
-					
-				}
-				if (oldQty > dataQty) {
-					//console.log('dataLayer remove');
-					dataLayer.push({
-						'event': 'removeFromCart',
-						'ecommerce': {
-							'currencyCode': 'USD',
-							'remove': {                                // 'add' actionFieldObject measures.
-								'products': [{                        //  adding a product to a shopping cart.
-									'name': $(v2Data).find('h3').text(),
-									'id': dLID,
-									'price': priceElement,
-									'brand': $(v2Data).find('.brand').text(),
-									//'category': 'Apparel',
-									//'variant': $(v2Data).attr('data-vid'),
-									'quantity': absChangeQty
-								}]
+						});
+
+					}
+					if (oldQty > dataQty) {
+						//console.log('dataLayer remove');
+						dataLayer.push({
+							'event': 'removeFromCart',
+							'ecommerce': {
+								'currencyCode': 'USD',
+								'remove': {                                // 'add' actionFieldObject measures.
+									'products': [{                        //  adding a product to a shopping cart.
+										'name': $(v2Data).find('h3').text(),
+										'id': dLID,
+										'price': priceElement,
+										'brand': $(v2Data).find('.brand').text(),
+										//'category': 'Apparel',
+										//'variant': $(v2Data).attr('data-vid'),
+										'quantity': absChangeQty
+									}]
+								}
 							}
-						}
-					});
-		
+						});
+
+					}
 				}
 			}
-		}
-		//
-		//
-		v2Data.find('.cart-total .price .val').text(amount);
-		v2Data.find('.cart-total .percent .val').text(parseFloat(savings).toFixed(0));
+			//
+			//
+			v2Data.find('.cart-total .price .val').text(amount);
+			v2Data.find('.cart-total .percent .val').text(parseFloat(savings).toFixed(0));
 
-		$('.estimate-subtotal .val').text(data.result.item_subtotal);
-		$('.rate-estimate-checkout .val').text(data.result.shipping_rate);
-		$('.estimate-coupons .val').text(data.result.discount_price);
-		if (data.result.discount_price > 0 ) { $('.estimate-coupons').collapse('show'); }
-		$('.estimate-bundles .val').text(data.result.bundle_discount);
-		if(typeof data.result.shipping_rate == 'undefined' ||  !data.result.shipping_rate  ) {
-			data.result.shipping_rate = 0.00;
-		}
-		if(data.result.shipping_rate!='') {
-			$('.rate-estimate-checkout').collapse('show');
-		} else {
-			$('.rate-estimate-checkout').collapse('hide');
-		}
-		if(data.result.tax_rate!=0 && data.result.shipping_rate!='') {
-			$('.total-before-tax').collapse('show');
-		} else {
-			$('.total-before-tax').collapse('hide');
-		}
-		if(data.result.tax_rate != 0) {
-			$('.tax-estimate-checkout').collapse('show');
-		} else {
+			$('.estimate-subtotal .val').text(data.result.item_subtotal);
+			$('.rate-estimate-checkout .val').text(data.result.shipping_rate);
+			$('.estimate-coupons .val').text(data.result.discount_price);
+			if (data.result.discount_price > 0) { $('.estimate-coupons').collapse('show'); }
+			$('.estimate-bundles .val').text(data.result.bundle_discount);
+			if (typeof data.result.shipping_rate == 'undefined' || !data.result.shipping_rate) {
+				data.result.shipping_rate = 0.00;
+			}
+			if (data.result.shipping_rate != '') {
+				$('.rate-estimate-checkout').collapse('show');
+			} else {
+				$('.rate-estimate-checkout').collapse('hide');
+			}
+			if (data.result.tax_rate != 0 && data.result.shipping_rate != '') {
+				$('.total-before-tax').collapse('show');
+			} else {
+				$('.total-before-tax').collapse('hide');
+			}
+			if (data.result.tax_rate != 0) {
+				$('.tax-estimate-checkout').collapse('show');
+			} else {
 
-			$('.tax-estimate-checkout').collapse('hide');
-		}
+				$('.tax-estimate-checkout').collapse('hide');
+			}
 
-		$('.total-before-tax .val').text((parseFloat(data.result.item_subtotal) + parseFloat(data.result.shipping_rate)).toFixed(2));
-		$('.tax-estimate-checkout .val').text(data.result.tax_rate);
-		$('.estimate-total .val').text(data.result.total);
-	});
+			$('.total-before-tax .val').text((parseFloat(data.result.item_subtotal) + parseFloat(data.result.shipping_rate)).toFixed(2));
+			$('.tax-estimate-checkout .val').text(data.result.tax_rate);
+			$('.estimate-total .val').text(data.result.total);
+
+			if (data.result.shipping_methods) {
+				$.each(data.result.shipping_methods, function (key, method) {
+					if (method.bottom_days_range && method.top_days_range) {
+						$('tr[data-method="' + method.id + '"] .wsnw').html(method.bottom_days_range + " - " + method.top_days_range + " days");
+					}
+					$('tr[data-method="' + method.id + '"] .shrink').html("$" + method.shipping_rate);
+				});
+			}
+		});
 }
 
 $('#wishlist-form .modal_list_quantity, #registry .modal_list_quantity').on('hidden.bs.modal', function () {
@@ -127,15 +136,15 @@ function adjustQuantity(qtyField, increment) {
 	var model = qtyField.data('model');
 
 	var min = qtyField.attr('min') ? qtyField.attr('min') : 1;
-	if(previousValue + increment <= min ){
+	if (previousValue + increment <= min) {
 		qtyField.val(min);
 	} else {
 		qtyField.val(previousValue += increment);
 	}
 	if (qtyField.val() <= min || qtyField.val() < 1) {
-		qtyField.parents('.input-group').find('.btn-remove').attr('disabled',true);
+		qtyField.parents('.input-group').find('.btn-remove').attr('disabled', true);
 	} else {
-		qtyField.parents('.input-group').find('.btn-remove').attr('disabled',false);
+		qtyField.parents('.input-group').find('.btn-remove').attr('disabled', false);
 	}
 
 	//Set qty to limit if entered value is above
@@ -146,16 +155,16 @@ function adjustQuantity(qtyField, increment) {
 		var formData = form.serialize(); // We must serialize our form data here because disabled fields are not submitted
 
 		// Dim quantity field while we update
-		qtyField.parents('.input-group').find('input,button').prop('disabled',true);
+		qtyField.parents('.input-group').find('input,button').prop('disabled', true);
 
 		if (typeof model === 'undefined') { // No model defined, so submit the entire form
 			$.ajax({
 				type: form.attr('method'),
 				url: form.attr('action'),
 				data: formData + '&action=update'
-			}).always(function() {
-				qtyField.parents('.input-group').find('input,button').prop('disabled',false);
-				if (qtyField.val() <= qtyField.attr('min') || qtyField.val() < 1) qtyField.parents('.input-group').find('.btn-remove').attr('disabled',true);
+			}).always(function () {
+				qtyField.parents('.input-group').find('input,button').prop('disabled', false);
+				if (qtyField.val() <= qtyField.attr('min') || qtyField.val() < 1) qtyField.parents('.input-group').find('.btn-remove').attr('disabled', true);
 			});
 		} else {
 			qtyField.parents('.item').find('.error').hide();
@@ -165,15 +174,15 @@ function adjustQuantity(qtyField, increment) {
 				url: acendaBaseUrl + '/api/' + model + '/' + id,
 				dataType: 'json',
 				data: JSON.stringify({ quantity: qtyField.val() })
-			}).always(function() {
-				qtyField.parents('.input-group').find('input,button').prop('disabled',false);
-				if (qtyField.val() <= qtyField.attr('min') || qtyField.val() < 1) qtyField.parents('.input-group').find('.btn-remove').attr('disabled',true);
-			}).fail(function(e) {
+			}).always(function () {
+				qtyField.parents('.input-group').find('input,button').prop('disabled', false);
+				if (qtyField.val() <= qtyField.attr('min') || qtyField.val() < 1) qtyField.parents('.input-group').find('.btn-remove').attr('disabled', true);
+			}).fail(function (e) {
 				data = $.parseJSON(e.responseText);
 				//console.log(data)
 				qtyField.val(previousValue -= increment);
 				if (data.code === 400 && model === 'cart/item') { // Bad request for the cart - not enough inventory
-					if(typeof data.error != 'undefined') {
+					if (typeof data.error != 'undefined') {
 						var error = data.error[Object.keys(data.error)[0]][0];
 						qtyField.parents('.item').find('.error').html(error);
 					} else {
@@ -182,19 +191,19 @@ function adjustQuantity(qtyField, increment) {
 				} else { // Probably a connection failure
 					qtyField.parents('.item').find('.error').html('Unknown error: could not update quantity.');
 				}
-				if(limit){
-				  if(!isNaN(limit)){
-					if(previousValue > limit){
-						//console.log('was too big, qualified, set to limit: '+limit);
-					  qtyField.val(limit);
+				if (limit) {
+					if (!isNaN(limit)) {
+						if (previousValue > limit) {
+							//console.log('was too big, qualified, set to limit: '+limit);
+							qtyField.val(limit);
+						}
 					}
-				  }
 				}
 				qtyField.parents('.item').find('.error').show();
 				if (model === 'cart/item') { // Check if we're at the cart, and if so, update the cart subtotal/individual item totals
 					updateCartTotals(qtyField, id);
 				}
-			}).done(function() {
+			}).done(function () {
 				if (model === 'cart/item') { // Check if we're at the cart, and if so, update the cart subtotal/individual item totals
 					updateCartTotals(qtyField, id);
 				}
@@ -202,41 +211,40 @@ function adjustQuantity(qtyField, increment) {
 		}
 	} else {
 		//console.log('detect not cart page')
-		if(qtyField.val() > limit) {
+		if (qtyField.val() > limit) {
 			//console.log('detect prod page val too high, adjust')
 			qtyField.val(limit);
 		}
 	}
-            	
+
 }
 
 
 // +/- buttons on single page and collections
-$(document).on('click','.btn-add', function(e) {
-    //console.log('ran 1')
+$(document).on('click', '.btn-add', function (e) {
+	//console.log('ran 1')
 	e.preventDefault();
 	adjustQuantity($(this).parent().parent().find('.quantity-selector'), 1);
 });
-$(document).on('click','.btn-remove', function(e) {
-    //console.log('ran 2')
+$(document).on('click', '.btn-remove', function (e) {
+	//console.log('ran 2')
 	e.preventDefault();
 	adjustQuantity($(this).parent().parent().find('.quantity-selector'), -1);
 });
 
-$(document).on('focusin','.quantity-selector', function() {
-    //console.log("Saving value " + $(this).val());
-    $(this).data('cur', $(this).val());
+$(document).on('focusin', '.quantity-selector', function () {
+	//console.log("Saving value " + $(this).val());
+	$(this).data('cur', $(this).val());
 });
 // Hitting the enter key on the add quantity fields
-$(document).on('change','.quantity-selector', function() {
-    //console.log('ran 3')
+$(document).on('change', '.quantity-selector', function () {
+	//console.log('ran 3')
 	adjustQuantity($(this), 0); // Quantity was adjusted externally
 });
-$(document).on('keypress','.quantity-selector', function(e) {
+$(document).on('keypress', '.quantity-selector', function (e) {
 	// Run adjust quantity action when numbers are entered in field
-	if (e.which == 13)
-	{
-	    //console.log('ran 4')
+	if (e.which == 13) {
+		//console.log('ran 4')
 		e.preventDefault();
 		adjustQuantity($(this), 0); // Quantity was adjusted externally
 	}
@@ -244,7 +252,7 @@ $(document).on('keypress','.quantity-selector', function(e) {
 
 var sendDLFirst = 1;
 
-$('.btn-remove-all').click(function(e) {
+$('.btn-remove-all').click(function (e) {
 	if (sendDLFirst == 1) {
 		e.preventDefault();
 		//console.log('gotcha');
@@ -285,6 +293,6 @@ $('.btn-remove-all').click(function(e) {
 	}
 });
 
-$(document).ready(function() {
-   updateCartTotals($(''),0);
+$(document).ready(function () {
+	updateCartTotals($(''), 0);
 })
